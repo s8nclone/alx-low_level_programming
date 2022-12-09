b95c5a543bbf2fb8fdafe8d5e8f34688a188ab8a
s8nclone <dayojnr.3@gmail.com>
2022-12-08 20:37:02 -0800

    helli, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,9 @@
 /**
  * main - entry point, print string in the print function
  *
+ * Description : using the main function
+ * this program prints "with proper grammar, but
+ * the outcome is a piece of art,"
  * Return : 0 (end the program)
  *
  */
