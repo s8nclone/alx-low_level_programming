99525149530fcad2e9738d1b0919b2eb576a3d01
s8nclone <dayojnr.3@gmail.com>
2022-12-08 20:31:13 -0800

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point, print string in the print function
+ *
+ * Return : 0 (end the program)
+ *
+ */
+int main(void)
+{
+	printf("%s", "with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
