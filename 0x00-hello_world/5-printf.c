9ff35617b28ac42f9fe87096540d4538dad9b89e
s8nclone <dayojnr.3@gmail.com>
2022-12-09 19:41:28 -0800

    hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  * Description : using the main function
  * this program prints "with proper grammar, but
  * the outcome is a piece of art,"
- * return : Always 0
+ * Return: Always 0
  */
 int main(void)
 {
