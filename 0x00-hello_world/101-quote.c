35235074a30e7bfa4d34f611852647b84b5e507b
s8nclone <dayojnr.3@gmail.com>
2022-12-09 20:00:38 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: the main function prints
+ * exactly "and that piece of art is useful ..."
+ * Return: 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
