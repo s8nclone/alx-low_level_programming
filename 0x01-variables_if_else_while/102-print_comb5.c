27604cccf619a7940055364238e8f1319bb14d54
s8nclone <dayojnr.3@gmail.com>
2022-12-09 19:02:21 -0800

    software is eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: the main program prints
+ * all possible cominations of two two-digit numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, j;
+	
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i; j <= 99; j++)
+		{
+			if (i != j)
+			{
+				putchar(i / 10 + 48);
+				putchar(i % 10 + 48);
+				putchar(' ');
+				putchar(j / 10 + 48);
+				putchar(j % 10 + 48);
+
+				if (i * 100 + j != 9899)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
