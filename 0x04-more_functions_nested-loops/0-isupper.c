aa80963f3ecce7db77214184736bc0acb19fd7e3
s8nclone <dayojnr.3@gmail.com>
2022-12-23 04:00:27 -0800

    isupper

--- 0x04-more_functions_nested-loops/0-isupper.c
+++ 0x04-more_functions_nested-loops/0-isupper.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * _isupper - check code
+ * _isupper - function declaration
  *
- * Description: checks for uppercase letters
+ * @c: character to check
  *
- * Return: Always 0
+ * Description: function to check for uppercase letters
+ *
+ * Result: 0 or 1
  */
 int _isupper(int c)
 {
