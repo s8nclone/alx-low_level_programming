6c87671c5962544066bbe98a6b7a8a1d89ab70b4
s8nclone <dayojnr.3@gmail.com>
2022-12-23 07:35:23 -0800

    always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - entry point function 
+ * @s: string
+ * 
+ * Description: function that changes all lowercase 
+ * letters of a string to uppercase
+ *
+ * Return: string
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i); i++)
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			*(s + i) = *(s + i) - 32;
+		}
+	}
+	return (s);
+}
