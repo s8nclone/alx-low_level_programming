106d20a627aabdab97609ec9858107df49bee0a0
s8nclone <dayojnr.3@gmail.com>
2022-12-23 11:21:06 -0800

    noise is a buffer, more effective tan cubicles or booth walls

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -14,41 +14,42 @@ void print_buffer(char *b, int size)
 {
 	int byte, index;
 
-	for (byte = 0; byte < size; byte += 10)
-	{
-		printf("%08x: ", byte);
-
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-			{
-				printf(" ");
-			}
-			else
-			{
-				printf("%02x", *(b + index + byte));
-			}
-			if ((index % 2) != 0 && index != 0)
-			{
-				printf(" ");
-			}
-		}
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-				break;
-			else if (*(b + index + byte) >= 31 &&
-					*(b + index + byte) <= 126)
-				printf("%c", *(b + index + byte));
-			else
-			{
-				printf(".");
-			}
-		}
-		if (byte >= size)
-			continue;
-		printf("\n");
-	}
 	if (size <= 0)
 		printf("\n");
+	else
+	{
+		for (byte = 0; byte < size; byte += 10)
+		{
+			printf("%08x: ", byte);
+			index = 0;
+			while (index < 10)
+			{
+				if (index % 2 == 0 && index != 0)
+				{
+					printf(" ");
+				}
+				if ((index + byte) > size - 1)
+				{
+					printf("  ");
+				}
+				else
+				{
+					printf("%.2x", b[byte + index]);
+				}
+				index++;
+			}
+			printf(" ");
+			index = 0;
+			while (index < 10)
+			{
+				if ((index + byte) > size - 1)
+					break;
+				if (b[index + byte] <= 31 || b[index + 1] >= '~')
+					b[index + byte] = '.';
+				putchar(b[index + byte]);
+				index++;
+			}
+			printf("\n");
+		}
+	}
 }
