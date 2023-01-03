61ed11ebc4e978c50a2edb0878344e85d968b898
s8nclone <dayojnr.3@gmail.com>
2023-01-02 22:10:38 -0800

    fizz-buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function entry
+ *
+ * Description: prints fizzbuzz for multiple of 3 and 5,
+ * fizz for muktiple of 3 and buzz for mutiple of 5
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	while (i < 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
