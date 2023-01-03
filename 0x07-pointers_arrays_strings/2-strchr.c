430d80b15283ab9a6fba8bc776e0b0b9c917ac20
s8nclone <dayojnr.3@gmail.com>
2023-01-02 17:33:47 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strchr - function name
+ * @s: pointer to string
+ * @c: character to check
+ *
+ * Description: locates a character in a string
+ *
+ * Return: pointer to character
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		else
+		{
+			return ("NULL");
+		}
+		s++;
+	}
+	return ('\0');
+}
