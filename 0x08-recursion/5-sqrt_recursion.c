c020c52299d1d26cfeea3ef1ed2150067ff0dd62
s8nclone <dayojnr.3@gmail.com>
2023-01-05 19:39:28 -0800

    your subconcious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * squarert - function name
+ * @n: int to find square root
+ * @val: square root
+ *
+ * Description: find square root
+ *
+ * Return: int
+ */
+
+int squarert(int n, int val)
+{
+	if (val * val == n)
+	{
+		return (val);
+	}
+	else if (val * val > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (squarert(n, val + 1));
+	}
+}
+
+
+/**
+ * _sqrt_recursion - function name
+ * @n: int
+ *
+ * Description: return the natural square root of a number
+ *
+ * Return: Always 0
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		return (squarert(2, n));
+	}
+}
