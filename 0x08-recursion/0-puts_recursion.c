8729d96e29307e67f05d26f1bdee881271e90860
s8nclone <dayojnr.3@gmail.com>
2023-01-05 18:49:03 -0800

    she locked away a secret, deep inside herself, something she once knew to be true..but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function name
+ * @s: pointer to string
+ *
+ * Description: prints a string, followed by a new line
+ *
+ * Return: Always 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
