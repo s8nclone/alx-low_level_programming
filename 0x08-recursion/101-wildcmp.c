fea8e32f68cdb6b397a7122ea22744f8509207e5
s8nclone <dayojnr.3@gmail.com>
2023-01-05 20:43:15 -0800

    inception. Now, befor you bother telling me it's impossible..

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * wildcmp - function name
+ * @s1: variable 1
+ * @s2: variable 2
+ *
+ * Description: copares two strings and returns 1 if
+ * the string can be considered identical, otherwise 0
+ *
+ * Return: Always 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	else if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+
+	if (*s2 == '*')
+	{
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+		{
+			return (1);
+		}
+		else if (*(s1 + 1) != '\0' && *s2 == '\0')
+		{
+			return (0);
+		}
+	}
+	return (0);
+}
