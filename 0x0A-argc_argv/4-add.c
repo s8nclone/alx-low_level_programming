e4ab53f0f08a622cded1d9ac498fac5a51090687
s8nclone <dayojnr.3@gmail.com>
2023-01-10 04:25:03 -0800

    to infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _intcheck - function
+ * @n: char to check
+ *
+ * Description: checks if a character is int
+ *
+ * Return: Always 0
+ */
+
+int _intcheck(char *n)
+{
+	int i, len;
+
+	i = 0;
+	len = strlen(n);
+	while (i < len)
+	{
+		if (n[i] < '0' || n[i] > '9')
+		{
+			return (-1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+
+/**
+ * main - function
+ * @argc: int
+ * @argv: list
+ *
+ * Description: adds positive numbers
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, sum;
+
+	sum = 0;
+	for (a = 1; a < argc; a++)
+	{
+		b = _intcheck(argv[a]);
+		if (b == -1)
+		{
+			printf("error\n");
+			return (1);
+		}
+		sum += b;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
