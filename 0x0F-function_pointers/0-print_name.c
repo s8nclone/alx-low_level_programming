a75947339b62a7571db90bf2c8e863a7a68a4c8f
s8nclone <dayojnr.3@gmail.com>
2023-01-23 10:24:32 -0800

    what's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function
+ * @name: name to be printed
+ * @f poiter to the function that prints the name
+ *
+ * Description: a function that prints a name
+ *
+ * Return: Always 0
+ */
+
+void print_name(char *name, void (*f)(car *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
