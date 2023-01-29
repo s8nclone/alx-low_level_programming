a655f86efa07308e6e12c017e9a3e6914b14c87a
s8nclone <dayojnr.3@gmail.com>
2023-01-29 04:00:45 -0800

    the hare and the tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,5 +7,5 @@
 void __attribute__ ((constructor)) premain()
 {
 	printf(
-		"You're beat! and yet, you must allow, \nI bore my house upon my back!\n");
+	"You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
