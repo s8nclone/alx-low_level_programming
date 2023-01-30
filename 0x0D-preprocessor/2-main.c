d0e6327d8af1a79cc8d96a8bde558735eee235de
s8nclone <dayojnr.3@gmail.com>
2023-01-30 06:38:38 -0800

    file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of complilation file 
+ * followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n, __BASE_FILE__);
+	return 0;
+}
