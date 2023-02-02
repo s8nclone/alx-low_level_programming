70a6575536e9146c02501690f6833134e07d0376
s8nclone <dayojnr.3@gmail.com>
2023-02-02 07:34:39 -0800

    0.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 
 /**
  * binary_to_uint - function
- * @b: pointer to strin of 0 and 1 chars
+ * @b: pointer to string of 0 and 1 chars
  *
  * Description: converts a binary number to an unsigned int
  *
@@ -13,27 +13,22 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int len = 0, i;
+	int i;
+	unsigned int num;
 
-	if (b == NULL)
-		return (num);
-
-	while (b[len] != '\0')
-		len++;
-	len -= 1;
-
-	i = 0;
-	while (b[i])
+	num = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (num);
-
-		if (b[i] == '1')
-			num += (1 * (1 << len));
-		i++;
-		len--;
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 	}
-
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
+	}
 	return (num);
 }
