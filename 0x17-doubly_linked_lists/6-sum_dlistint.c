86fa4e95f36a4ad13626e97deb192d94083e40e4
s8nclone <dayojnr.3@gmail.com>
2023-03-17 11:31:28 -0700

    sum list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -13,10 +13,17 @@ int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
 
-	while (head)
+	if (head != NULL)
 	{
-		sum += head->n;
-		head = head->next;
+		while (head->prev != NULL)
+			head = head->prev;
+
+		while (head != NULL)
+		{
+			sum =+ head->n;
+			head = head->next;
+		}
 	}
-	return (sum)
+
+	return (sum);
 }
