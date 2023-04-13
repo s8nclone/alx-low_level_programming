4051d661a19eb331855e83cd74cf797d0c1395f2
s8nclone <dayojnr.3@gmail.com>
2023-04-12 18:24:36 -0700

    key -> index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - function
+ * @key: key passed
+ * @size: size of the hash table
+ *
+ * Description: generates a distributed index according
+ *		to a given hash
+ *
+ * Return: key index
+ */
+
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	if (key == NULL || size == 0)
+		return (0);
+	return (hash_djb2(key) % size);
+}
