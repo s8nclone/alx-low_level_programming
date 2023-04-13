d91a2f8ba56ebe47687cac170dae105ecaa64154
s8nclone <dayojnr.3@gmail.com>
2023-04-12 18:15:24 -0700

    djb2

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - function
+ * @str: string
+ *
+ * Description: generates a random number through a given
+ * 		string (hash number)
+ *
+ * Return: hash number
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+	return (hash);
+}
