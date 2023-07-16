f79efc7d336c727b850b8c8ea9126267f3b0e9ef
s8nclone <dayojnr.3@gmail.com>
2023-07-16 00:54:57 -0700

    advanced binary search

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,47 +1,58 @@
 #include "search_algos.h"
 
 /**
- * jump_list - Function
- *
- * @list: A pointer to the  head of the linked list to search.
- * @size: The number of nodes in the list.
- * @value: The value to search for.
- *
- * Return: If the value is not present or the head of the list is NULL, NULL.
- *         Otherwise, a pointer to the first node where the value is located.
- *
- * Description: Searches for an algorithm in a sorted singly linked list of int
- *		Prints a value every time it is compared in the list.
- *              Uses the square root of the list size as the jump step.
- */
+  * advanced_binary_recursive - Function
+  *
+  * @array: A pointer to the first element of the [sub]array to search.
+  * @left: The starting index of the [sub]array to search.
+  * @right: The ending index of the [sub]array to search.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Searches recursively for a value in a sorted array of integers
+  *		 Prints the array or subarray being searched after each change.
+  */
 
-listint_t *jump_list(listint_t *list, size_t size, int value)
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
 {
-	size_t step, step_size;
-	listint_t *node, *jump;
+	size_t i;
 
-	if (list == NULL || size == 0)
-		return (NULL);
+	if (right < left)
+		return (-1);
 
-	step = 0;
-	step_size = sqrt(size);
-	for (node = jump = list; jump->index + 1 < size && jump->n < value;)
-	{
-		node = jump;
-		for (step += step_size; jump->index < step; jump = jump->next)
-		{
-			if (jump->index + 1 == size)
-				break;
-		}
-		printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
-	}
+	printf("Searching in array: ");
+	for (i = left; i < right; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
 
-	printf("Value found between indexes [%ld] and [%ld]\n",
-			node->index, jump->index);
+	i = left + (right - left) / 2;
+	if (array[i] == value && (i == left || array[i - 1] != value))
+		return (i);
+	if (array[i] >= value)
+		return (advanced_binary_recursive(array, left, i, value));
+	return (advanced_binary_recursive(array, i + 1, right, value));
+}
 
-	for (; node->index < jump->index && node->n < value; node = node->next)
-		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
-	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+/**
+  * advanced_binary - Function
+  *
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the first index where the value is located.
+  *
+  * Description: Searches for a value in a sorted array of integers and
+  *		 Prints the array or subarray being searched after each change.
+  */
 
-	return (node->n == value ? node : NULL);
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (array == NULL || size == 0)
+		return (-1);
+
+	return (advanced_binary_recursive(array, 0, size - 1, value));
 }
